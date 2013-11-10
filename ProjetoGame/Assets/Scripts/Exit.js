26374a059e188efb5fe2f03de3fbc3a9d2fc0cdf
loxorolim <loxorolim@gmail.com>
2013-11-10 19:35:33 -0200

    Algumas melhorias

---
+++ ProjetoGame/Assets/Scripts/Exit.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+//var newGame : GameObject;
+//var howToPlay : GameObject;
+//var credits : GameObject;
+//var exit : GameObject;
+function Start () {
+//    newGame = GameObject.Find("NovoJogo");
+//    howToPlay = GameObject.Find("HowtoPlay");
+//    credits = GameObject.Find("Creditos");
+//    exit = GameObject.Find("Sair");
+}
+
+function Update () {
+//    newGame = GameObject.Find("NovoJogo");
+//    howToPlay = GameObject.Find("HowtoPlay");
+//    credits = GameObject.Find("Creditos");
+//    exit = GameObject.Find("Sair");
+
+}
+
+function OnMouseDown (){
+//    if (newGame){
+//        Application.LoadLevel(1);
+//    }else if(howToPlay){
+//        Application.LoadLevel(2);
+//    }else if(credits){
+//        Application.LoadLevel(3);
+//    }else if(exit){
+        Application.Quit();
+        Debug.Log("teste");
+//    }
+    
+}
---
+++ ProjetoGame/Assets/Scripts/Return.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+var life : int = 3;
+var fall : boolean;
+function Start () {
+
+}
+
+function Update () {
+if(transform.position.y <= -7){
+	transform.position.x = -3.911;
+	transform.position.z = 3;
+	transform.position.y = 3;
+	fall = true;
+}else fall = false;
+if (fall == true)life -=1;
+print(life);
+if (life < 1)Application.LoadLevel("Menu");
+}
